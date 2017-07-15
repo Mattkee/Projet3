07b989f9bc7b3c4a0bfdc7a1b4ffc7e570c9e90e
Mattkee <tkee@hotmail.fr>
2017-07-15 11:20:17 +0200

    New Character created

---
+++ Game.swift
@@ -0,0 +1,23 @@
+class Character {
+    var name : String
+    var health : Int
+    var attack : Int
+    
+    init(name : String, health : Int, attack : Int) {
+        self.name = name
+        self.health = health
+        self.attack = attack
+    }
+}
+class Warrior : Character {
+    
+}
+class Wizard : Character {
+    
+}
+class Colossus : Character {
+    
+}
+class Dwarf : Character {
+    
+}
