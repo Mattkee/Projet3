a9cdc36666341d08f63c2389136feb46514969d4
Mattkee <tkee@hotmail.fr>
2017-07-17 10:33:16 +0200

    add new characteristic for each type of Characters

--- Game.swift
+++ Game.swift
@@ -1,4 +1,5 @@
 class Character {
+    // création des paramètres commun à tous les personnages.
     var name : String
     var health : Int
     var attack : Int
@@ -9,15 +10,38 @@ class Character {
         self.attack = attack
     }
 }
+
 class Warrior : Character {
-    
+    // création du personnage de type guerrier.
+    var epee = 10
+    init(name : String) {
+        super.init(name: name, health: 100, attack: 10)
+        self.attack = attack
+        attack = attack + epee
+    }
 }
 class Wizard : Character {
-    
+    // création du personnage de type Mage.
+    func soign () {
+        
+    }
+    var magie = 100
+    init(name : String){
+        super.init(name: name, health: 60, attack: 2)
+    }
 }
 class Colossus : Character {
-    
+    // création du personnage de type Colosse.
+    init(name : String){
+        super.init(name: name, health: 500, attack: 3)
+    }
 }
 class Dwarf : Character {
-    
+    // création du personnage de type Nain.
+    var hache = 50
+    init(name : String){
+        super.init(name: name, health: 30, attack: 5)
+        self.attack = attack
+        attack = attack + hache
+    }
 }
