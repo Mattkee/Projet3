66cb3e00b08d8160108f8ff407e7454a7839927c
Mattkee <tkee@hotmail.fr>
2017-08-02 17:58:20 +0200

    " add new func nameCharacter"

--- Projet3/Game.swift
+++ Projet3/Game.swift
@@ -1,12 +1,13 @@
 //The game class will allow us to manage the different stages of the game
 class Game {
     var players = [Player]()
-    
+    var playerNumber = 0
     //First stage of the game we'll ask the players to choose the name of their team and choose three characters
     
     public func initializeGame() {
         print("Bienvenue dans notre super jeu de combat.")
         for i in 0 ... 1 {
+            playerNumber += 1
             self.initializeTeamsName(playerNumber: i)
             self.initializeTeamsCharacters(playerNumber: i)
         }
@@ -14,7 +15,7 @@ class Game {
     private func initializeTeamsName(playerNumber : Int){
         
         
-        let player = Player()
+        let team = Player()
         
         print("Joueur \(playerNumber + 1) Quel est le nom de votre equipe")
 
@@ -24,8 +25,8 @@ class Game {
             name = readLine()
         } while((name?.characters.count)! <= 3)
         
-        player.setTeamName(name : name!)
-        players.append(player)
+        team.setTeamName(name : name!)
+        players.append(team)
         
  // changer la boucle pour qu'elle soit plus compréhensible, en démarrant de 1 et en changer l'appelation i.
         
@@ -36,6 +37,7 @@ class Game {
     private func initializeTeamsCharacters(playerNumber : Int){
         // Il faudra demander à chaque équipe de choisir 3 personnages parmis la liste
         //des types de personnages proposés : 1. Combattant 2. Mage 3. Colosse 4. Nain
+        let character = Character(health : 0 , attack : 0)
         
         while players[playerNumber].getTeamMembersNumber() < 3 {
             print(" Joueur \(playerNumber + 1) : Choisissez le personnage \(players[playerNumber].getTeamMembersNumber() + 1) de votre équipe parmis les choix suivants : 1. Combattant 2. Mage 3. Colosse 4. Nain")
@@ -44,52 +46,47 @@ class Game {
             switch choixPersonnage! {
             case 1:
                 // remplacer par une methode nameCharacter.
-                var name : String? = "Character"
-                repeat{
-                    name = String(readLine()!)
-                } while((name?.characters.count)! <= 3)
+                
+                let name = character.nameCharacter()
                 
                 let personnage = Warrior()
-                personnage.name = name!
-                players[playerNumber].setTeamMember(name : name! , member: personnage)
                 
-                print("Votre personnage \(players[playerNumber].getTeamMembersNumber()) est un combattant du nom de \(name!)")
+                players[playerNumber].setTeamMember(name : name , member: personnage)
+                
+                print("Votre personnage \(players[playerNumber].getTeamMembersNumber()) est un combattant du nom de \(name)")
                 print(" le nombre de personnage est \(players[playerNumber].getTeamMembersNumber())")
+                
             case 2:
-                var name : String? = "Character"
-                repeat{
-                    name = String(readLine()!)
-                } while((name?.characters.count)! <= 3)
+                
+                let name = character.nameCharacter()
                 
                 let personnage = Wizard()
-                personnage.name = name!
-                players[playerNumber].setTeamMember(name : name! , member: personnage)
                 
-                print("Votre personnage \(players[playerNumber].getTeamMembersNumber()) est un Mage du nom de \(name!)")
+                players[playerNumber].setTeamMember(name : name , member: personnage)
+                
+                print("Votre personnage \(players[playerNumber].getTeamMembersNumber()) est un Mage du nom de \(name)")
                 print(" le nombre de personnage est \(players[playerNumber].getTeamMembersNumber())")
+                
             case 3:
-                var name : String? = "Character"
-                repeat{
-                    name = String(readLine()!)
-                } while((name?.characters.count)! <= 3)
+                
+                let name = character.nameCharacter()
                 
                 let personnage = Colossus()
-                personnage.name = name!
-                players[playerNumber].setTeamMember(name : name! , member: personnage)
                 
-                print("Votre personnage \(players[playerNumber].getTeamMembersNumber()) est un Colosse du nom de \(name!)")
+                players[playerNumber].setTeamMember(name : name , member: personnage)
+                
+                print("Votre personnage \(players[playerNumber].getTeamMembersNumber()) est un Colosse du nom de \(name)")
                 print(" le nombre de personnage est \(players[playerNumber].getTeamMembersNumber())")
+                
             case 4:
-                var name : String? = "Character"
-                repeat{
-                    name = String(readLine()!)
-                } while((name?.characters.count)! <= 3)
+                
+                let name = character.nameCharacter()
                 
                 let personnage = Dwarf()
-                personnage.name = name!
-                players[playerNumber].setTeamMember(name : name! , member: personnage)
                 
-                print("Votre personnage \(players[playerNumber].getTeamMembersNumber()) est un Nain du nom de \(name!)")
+                players[playerNumber].setTeamMember(name : name , member: personnage)
+                
+                print("Votre personnage \(players[playerNumber].getTeamMembersNumber()) est un Nain du nom de \(name)")
                 print(" le nombre de personnage est \(players[playerNumber].getTeamMembersNumber())")
             default:
                 print("Je n'ai pas compris")
--- Projet3/Projet3/gameCharacter.swift
+++ Projet3/Projet3/gameCharacter.swift
@@ -1,13 +1,24 @@
 class Character {
     // création des paramètres commun à tous les personnages.
-    var name : String = ""
+    //var name : String = ""
     var health : Int
     var attack : Int
     
     init(health : Int, attack : Int) {
-        self.name = ""
+        //self.name = ""
         self.health = health
         self.attack = attack
+    }
+    func nameCharacter() -> String {
+        var name : String? = "Character"
+        
+        repeat{
+            name = String(readLine()!)
+        } while((name?.characters.count)! <= 3)
+        
+        name = name!
+        
+        return name!
     }
 }
 // créer une methode namecharacter
