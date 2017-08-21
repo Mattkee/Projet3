8f4c6656af7ba5b589b75d581a8e7492d00f2ee2
Mattkee <tkee@hotmail.fr>
2017-08-21 20:22:17 +0200

    created new class Fight

--- Projet3/Projet3/gameFight.swift
+++ Projet3/Projet3/gameFight.swift
@@ -1,3 +1,23 @@
 class Fight {
+    var playerNumber : Int = 0
+    var playerSelected : String = ""
+    var battleCharacter = [Character]()
     
+    func battle() {
+        playerSelected = game.playersName[playerNumber]
+        
+        for _ in 1 ... 2 {
+            for (name, _) in game.players {
+                let teamSelectedCharacter = [Character]()
+                for player in game.players.values {
+                    if playerSelected = name {
+                teamSelectedCharacter.append(
+                    print("joueur \(playerSelected) avec lequel de vos personnage voulez vous affronter votre adversaire"
+                    + "\n1. \(game.players[playerSelected]!) \(teamSelectedCharacter) "
+                    + "\n2. Mage"
+                    + "\n3. Colosse")
+                }
+            }
+        }
+    }
 }
