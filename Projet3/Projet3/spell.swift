1dfea575a26ca10497414db294b72bb88b8aebbb
Mattkee <tkee@hotmail.fr>
2017-09-11 08:31:55 +0200

    created new class Spell.

---
+++ Projet3/Projet3/spell.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+class Spell {
+    
+    var name : String
+    var magicPointCost : Int
+    
+    init(name: String , magicPointCost : Int) {
+        self.name = name
+        self.magicPointCost = magicPointCost
+    
+    }
+}
